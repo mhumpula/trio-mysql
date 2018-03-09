8dc47894b447fa2b2d61e93f4004152983bf1861
Matthias Urlichs <matthias@urlichs.de>
2018-03-09 20:10:58 +0100

    rewrite fixture
    be an object

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,30 +1,33 @@
 import pytest
-import pytest_trio
 import inspect
+from trio.testing import trio_test
+
+class _Setup:
+    def __init__(self, obj):
+        self.obj = obj
+        self.cleans = []
+
+    async def setup(self, obj):
+        self.obj = obj
+        await obj.setUp()
+
+    async def teardown(self):
+        await self.obj.tearDown()
+        while self.cleans:
+            p,a,k = self.cleans.pop()
+            await p(*a,**k)
+
+@pytest.fixture
+async def set_me_up():
+    setup = _Setup()
+    try:
+        yield setup
+    finally:
+        await setup.teardown()
 
 # auto-trio-ize all async functions
 @pytest.hookimpl(tryfirst=True)
 def pytest_pyfunc_call(pyfuncitem):
     if inspect.iscoroutinefunction(pyfuncitem.obj):
-        pyfuncitem.obj = pytest.mark.trio(pyfuncitem.obj)
-
-@pytest.fixture
-async def set_me_up():
-    obj = None
-    cleans = []
-    def addCleanup(p,*a,**k):
-        cleans.append((p,a,k))
-        
-    async def fn(self):
-        nonlocal obj
-        obj = self
-        obj.addCleanup = addCleanup
-        await self.setUp()
-
-    yield fn
-
-    await obj.tearDown()
-    while cleans:
-        p,a,k = cleans.pop()
-        await p(*a,**k)
+        pyfuncitem.obj = trio_test(pyfuncitem.obj)
 
