8489420c7657d6c3d78d5d6d430f261c3ced36db
Matthias Urlichs <matthias@urlichs.de>
2018-03-10 20:40:17 +0100

    wrap socket

--- trio_mysql/connections.py
+++ trio_mysql/connections.py
@@ -972,6 +972,8 @@ class Connection(object):
                     if DEBUG: print('connected using socket')
                     sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
                 sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+            elif not isinstance(sock, trio.SocketStream):
+                sock = trio.SocketStream(sock)
             self._sock = sock
             self._next_seq_id = 0
 
